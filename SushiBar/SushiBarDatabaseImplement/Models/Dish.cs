d333ecad7110ec0f7dc6726e1da95d0275c65584
Dmitry <alisho.ru@mail.ru>
2022-03-11 22:09:22 +0400

    3rd laboratory work is done

---
+++ SushiBar/SushiBarDatabaseImplement/Models/Dish.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SushiBarDatabaseImplement.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        [Required]
+        public string DishName { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [ForeignKey("DishId")]
+        public virtual List<DishIngredient> DishIngredients { get; set; }
+        [ForeignKey("DishId")]
+        public virtual List<Order> Orders { get; set; }
+    }
+}
---
+++ SushiBar/SushiBarDatabaseImplement/Models/Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SushiBarDatabaseImplement.Models
+{
+    /// <summary>
+    /// Ингредиент, требуемый для приготовления блюда
+    /// </summary>
+    public class Ingredient
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string IngredientName { get; set; }
+
+        [ForeignKey("IngredientId")]
+        public virtual List<DishIngredient> DishIngredients { get; set; }
+    }
+}
